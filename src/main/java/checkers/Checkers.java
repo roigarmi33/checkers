eac220a5dd300541397705dd68e7945ea22b0b85
roigarmi33 <Ironia1981>
2019-04-27 16:56:27 +0300

    Second Fix - Critical code smell on Move class fixed

--- src/main/java/checkers/Checkers.java
+++ src/main/java/checkers/Checkers.java
@@ -4,12 +4,17 @@ import exceptions.IncorrectMoveException;
 import exceptions.IncorrectMoveFormat;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Checkers {
 
 	public static void main(String[] args)
             throws IncorrectMoveFormat, IncorrectMoveException, IOException, ClassNotFoundException {
-	    (new Menu()).start();
+
+
+
+		(new Menu()).start();
 	}
 
 }
